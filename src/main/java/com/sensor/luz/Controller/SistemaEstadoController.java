4584ef85535142bba8908f8f993d869cc5175440
Gustavo Henrique Fiamoncini <gustavohf092006@gmail.com>
2026-04-28 21:45:15 -0300

    Lógica adicionada

--- src/main/java/com/sensor/luz/Controller/SistemaEstadoController.java
+++ src/main/java/com/sensor/luz/Controller/SistemaEstadoController.java
@@ -1,8 +1,31 @@
 package com.sensor.luz.Controller;
 
-import org.springframework.stereotype.Controller;
+import java.util.Map;
 
-@Controller
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.sensor.luz.Service.SistemaEstadoService;
+
+@RestController
+@RequestMapping("/")
+
 public class SistemaEstadoController {
-    
+
+    @Autowired
+    private SistemaEstadoService service;
+
+    private int ultimaLuminosidade;
+
+    @PostMapping("/sensor/luminosidade")
+    public String receberLuminosidade(@RequestBody Map<String, Integer> body) {
+        int valor = body.get("valor");
+        ultimaLuminosidade = valor;
+
+        return service.processarLuminosidade(valor);
+    }
+
 }
--- src/main/java/com/sensor/luz/Service/SistemaEstadoService.java
+++ src/main/java/com/sensor/luz/Service/SistemaEstadoService.java
@@ -4,5 +4,21 @@ import org.springframework.stereotype.Service;
 
 @Service
 public class SistemaEstadoService {
-    
+
+    private String modo = "AUTOMATICO";
+    private int threshold = 500;
+    private boolean ledLigado = false;
+
+    public String processarLuminosidade(int valor) {
+
+        if (modo.equals("AUTOMATICO")) {
+            if (valor < threshold) {
+                ledLigado = true;
+            } else {
+                ledLigado = false;
+            }
+        }
+
+        return ledLigado ? "ON" : "OFF";
+    }
 }
